91e65b709d3ae8f0926c92d1f9be75e663441754
Top <Top@8-17>
2024-10-14 21:29:42 +0500

    Добавьте файлы проекта.

---
+++ Cript.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CesarCript
+{
+    internal class Cript
+    {
+        private static List<char> _symbols = new List<char>(); // Коллекция для хранения символов для кодировки
+        private static int _shift = 3; // сдвиг шифрования
+        public static void InitDictionary() // Запонит List символами для шифрования
+        {            
+            for (char i = ' '; i <= 'z'; i++)
+            {                
+                _symbols.Add(i);
+            }            
+            for (char i = 'А'; i <= 'я'; ++i)
+            {             
+                _symbols.Add(i);
+            }                        
+        }
+        // Метод шифрования, возвращает шифрованный символ, учитывая те, которые не выдаются 
+        // обычным сдвигом
+        public static char Encrypt(char input) 
+        {
+            char result = '\0'; // пустой символ            
+            int index = _symbols.IndexOf(input);
+            if (index > _symbols.Count - 1 - _shift) 
+            {
+                result = _symbols[index%_symbols.Count];
+            }
+            else
+            {
+                result = _symbols[index + _shift];
+            }
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CesarCript
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Привет!";
+            Cript.InitDictionary();
+            string cryptoText = "";
+            foreach (var item in text)
+            {
+                cryptoText += Cript.Encrypt(item);
+            }
+            Console.WriteLine(text);
+            Console.WriteLine(cryptoText);
+        }
+    }
+}
